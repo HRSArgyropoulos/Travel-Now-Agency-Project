1764dddb45f564313f8bd0454f920059ea97a3cb
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-01-25 11:09:16 +0200

    gulp-css-media-queries

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,20 +1,40 @@
-const gulp = require('gulp');
+const { parallel } = require("gulp");
+const gulp = require("gulp");
 
 // plugins
-const imagemin = require('gulp-imagemin');
+const imagemin = require("gulp-imagemin");
+const zip = require("gulp-zip");
+const mq = require("gulp-group-css-media-queries");
 
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
-exports.default = defaultTask
+gulp.task("css-mq", () => {
+  return gulp.src("../css/*.css")
+  .pipe(mq())
+  .pipe(gulp.dest('dist/css'))
+});
 
-gulp.task('compress-imgs' , () => {
-  return gulp.src('../images/*/*')
-  .pipe(imagemin({
-    progressive: true,
-    optimizationLevel: 4
-  }))
-  .pipe(gulp.dest('../afterimages'));
-})
+gulp.task("compress-imgs", () => {
+  return gulp
+    .src("../images/*/*")
+    .pipe(
+      imagemin({
+        progressive: true,
+        optimizationLevel: 4,
+      })
+    )
+    .pipe(gulp.dest("dist/afterimages"));
+});
 
+gulp.task("zip", () => {
+  return gulp
+    .src([
+      "../*.html",
+      "../assets/*",
+      "../css/*",
+      "../images/*/*",
+      "../js/*"],
+      {base: "../"})
+    .pipe(zip("cpanel.zip"))
+    .pipe(gulp.dest("dist"));
+});
+
+exports.default = parallel("compress-imgs", "zip");
