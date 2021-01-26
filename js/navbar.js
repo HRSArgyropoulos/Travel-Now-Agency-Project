80ebb80e03366e1d249ef404325ce14c7c4bb137
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-01-26 23:16:41 +0200

    fixed-navbar-for-all-sub-pages

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,23 @@
+"use strict";
+
 window.onload = function () {
     const nav = document.getElementById('navbar');
-    const dest = document.getElementById('top-dest');
     const footer = document.getElementById('footer');
-    console.log(footer);
     
-    const start = dest.offsetTop-100;
+    let start;
+    const homepage = document.querySelector("#homepage");
+    if (homepage !== null) {
+        start = document.getElementById('top-dest');
+    } else {
+        start = document.getElementsByClassName("sub-page-top")[0];
+        nav.classList.add('sticky-nav-subpage');
+    }
+
+    start = start.offsetTop-100;
+    console.log(start);
+    //console.log(start);
     const end = footer.offsetTop-100;
+    //console.log(end);
     window.onscroll = () => {
         if (window.pageYOffset >= start && window.pageYOffset <= end) {
             nav.classList.add('sticky-nav');
