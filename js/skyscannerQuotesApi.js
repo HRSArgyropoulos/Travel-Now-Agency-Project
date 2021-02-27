7a2b874dc3174b873b441a867fb7f91d5cb08216
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-02-27 11:30:13 +0200

    comments-added

--- js/skyscannerQuotesApi.js
+++ js/skyscannerQuotesApi.js
@@ -1,80 +1,83 @@
 "use strict";
 
-const SKYURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices";
+const SKYURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices"; //skyscanner api base url
 
-const requestQuotes = (location,test) => {
-    fetch(test, {
+const requestQuotes = (location,url) => { //get quote(s) for a specific location
+    flightPromises.push(    //push promise into flightPromises array - see getQuotes function
+    fetch(url, {    //request quotes from api
         "method" : "GET",
         "headers" : {
             "x-rapidapi-key": "a8ad4f7fedmshc135d85aa401bc8p106b6bjsna3d6d4d974bf",
             "x-rapidapi-host" : "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-            "Accept" : "application/json"
+            "Accept" : "application/json" //return data in json formal (xml also available)
         }
     })
     .then(response => response.json())
     .then(flights => {
-        console.log(flights);
-        let flightsList = document.querySelector(`#${location} .flightQuotes ul`);
+        //console.log(flights);
+        let flightsList = document.querySelector(`#${location} .flightQuotes ul`); //get list to add flight quotes
         flights.Quotes.forEach(quote => {
-            let quoteLi = document.createElement("li");
-
+            let quoteLi = document.createElement("li"); //n-th quote
+            //Origin
             let departureIcon = document.createElement("i");
             departureIcon.classList.add("fas","fa-plane-departure");
             quoteLi.appendChild(departureIcon);
-
             let originLi = document.createElement("span");
             originLi.innerText = quoteQuery.originPlace;
             quoteLi.appendChild(originLi);
-
+            //Destination
             let arrivalIcon = document.createElement("i");
             arrivalIcon.classList.add("fas","fa-plane-arrival");
             quoteLi.appendChild(arrivalIcon);
-
             let destinationLi = document.createElement("span");
             destinationLi.innerText = quoteQuery.destinationPlace;
             quoteLi.appendChild(destinationLi);
-
+            //Minimum Price
             let priceLi = document.createElement("span");
-            priceLi.innerText = quote.MinPrice + " ₤";
+            priceLi.innerText = quote.MinPrice + " ₤"; //can also get it from Quote currency object
             quoteLi.appendChild(priceLi);
 
-            flightsList.appendChild(quoteLi);
+            flightsList.appendChild(quoteLi); //and quote to flighs list
         });
     })
-
+    );
 
 }
 
-const quoteQuery = { //QUOTE INFO
+const quoteQuery = { //CONST QUOTE INFO
     "country" : "UK",
     "currency" : "GBP",
     "locale" : "en-GB",
-    "originPlace" : "LHR", //LHR-iata
-    "destinationPlace" : "", //AMS-iata
-    "outboundPartialDate" : "2021-03-11", //YYYY-MM-DD
-    "inboundPartialDate" : "" // empty for one-way OPTIONAL
+    "originPlace" : "LHR", // IATA format
+    "destinationPlace" : "", // IATA format - this value will change depending on the destination
+    "outboundPartialDate" : "2021-03-11", //YYYY-MM-DD format - for now we ll leave it as is
+    "inboundPartialDate" : "" // empty for one-way (OPTIONAL) - this value will change depending on the destination (days of package)
 }
 
+const flightPromises = []; //will hold all of our promises from fetch requests to the skyscanner api
 
 
 const getQuotes = (destination) => {
-    const tab = document.querySelector(`#${destination.location} nav ul li:last-child`);
+    const tab = document.querySelector(`#${destination.location} nav ul li:last-child`); //price tab of destination
         tab.addEventListener("click", () => {
-            if (document.querySelector(`#${destination.location} .flightQuotes ul`).innerHTML===""){
-                let quoteURL = SKYURL + "/browsequotes/v1.0"; //quotes endpoint
-                quoteQuery.destinationPlace = `${destination.iata}`;
-                let outboundDay = parseInt(quoteQuery.outboundPartialDate.slice(8,10))+destination.days;
-                quoteQuery.inboundPartialDate = quoteQuery.outboundPartialDate.slice(0,8)+outboundDay;
-                for (const parameter in quoteQuery) {
-                    quoteURL += `/${quoteQuery[parameter]}`;
-                }
-                requestQuotes(destination.location,quoteURL);
+            if (document.querySelector(`#${destination.location} .flightQuotes ul`).innerHTML===""){ //if list is empty (no results added yet)
+                Promise.allSettled(flightPromises).then(() => { //ensure that the previous fetch requests have been "fullfilled" - wait / async
+                    let quoteURL = SKYURL + "/browsequotes/v1.0"; //quotes endpoint
+                    quoteQuery.destinationPlace = `${destination.iata}`;
+                    let outboundDay = parseInt(quoteQuery.outboundPartialDate.slice(8,10))+destination.days; //get "DD" from date and add the days from the destination data
+                    //here for simplicity we wont check if the date created will be valid
+                    quoteQuery.inboundPartialDate = quoteQuery.outboundPartialDate.slice(0,8)+outboundDay; // get "YYYY-MM-" and concatenate it with "DD"
+                    for (const parameter in quoteQuery) { //create url from which to fetch - we could do part of this outside of getQuotes
+                        quoteURL += `/${quoteQuery[parameter]}`;
+                    }
+                    requestQuotes(destination.location,quoteURL);
+                })
             }
         });
 };
 
 
-fetch("./destinations-data.json")
+fetch("./destinations-data.json") //get destinations data (JSON)
 .then(response => response.json())
 .then(destinations => {
     for (const destination of destinations) {
